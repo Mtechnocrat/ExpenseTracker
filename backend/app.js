b47f25dc3b115116426af8b3547caa593d817b7b
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-08 18:20:03 +0530

    Code update

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const cors=require('cors');
+const { db } = require('./db/db');
 const app=express()
 
 require('dotenv').config()
@@ -13,6 +14,7 @@ app.use(cors());
 
 
 const server=()=>{
+    db();
     app.listen(PORT,() =>{
         console.log("Listening to port : ",PORT);
     })
---
+++ backend/db/db.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+
+const db=async()=> {
+    try {
+        mongoose.set('strictQuery',false)
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('db connected');
+    } catch (error) {
+        console.log('DB connection error');
+    }
+    
+}
+
+module.exports = {db};
