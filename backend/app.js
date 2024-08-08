af9435e925b2d8d24bd9a682e4fa850f88334938
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-08 15:08:49 +0530

    First commit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+// const express=require('express');
+// const cors=require('cors');
+// const app=express()
+require('dotenv').config()
+const  PORT=process.env.PORT
+
+
+// //middlewares
+// app.use(express.json()); //we want our data to be in jSon
+// app.use(cors());
+
+
+const server=()=>{
+
+    console.log('Listening to port:',PORT);
+}
+
+server();
