9fbe171b58b3cfbbaf20c6673a3e4bd0d6ff753c
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-08 20:14:53 +0530

    expenses adding,viewing and deleting added

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,3 +1,4 @@
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router();
@@ -5,6 +6,9 @@ const router = require('express').Router();
 router.post('/add-income',addIncome)
     .get('/get-incomes',getIncomes)
     .delete('/delete-income/:id',deleteIncome)
+    .post('/add-expense',addExpense)
+    .get('/get-expense',getExpense)
+    .delete('/delete-expense/:id',deleteExpense)
 
 module.exports=router;
 
